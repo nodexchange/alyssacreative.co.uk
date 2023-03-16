2e36979198810e48429664353b9918d9be6cdaf3
Danny <barakadan421@gmail.com>
2023-03-16 16:02:24 +0300

    Pricing component

--- components/Pricing.js
+++ components/Pricing.js
@@ -5,7 +5,7 @@ function Pricing() {
     <section class="py-10 bg-gray-50 sm:py-16 lg:py-24">
     <div class="px-4 mx-auto max-w-7xl sm:px-6 lg:px-8">
         <div class="max-w-2xl mx-auto text-center">
-            <h2 class="text-3xl font-bold leading-tight text-black sm:text-4xl lg:text-5xl">Pricing & Plans</h2>
+            <h2 class="leading-tight text-black font-bold text-xl md:text-2xl lg:text-4xl">Pricing & Plans</h2>
             <p class="max-w-lg mx-auto mt-4 text-base leading-relaxed text-gray-600">Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis.</p>
         </div>
 
