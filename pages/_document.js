da737d35c12ab2092a8bd82c4bca17d48a5c851e
Mars <nodexchange@outlook.com>
2023-09-15 16:47:46 +0100

    seo keywords

--- pages/_document.js
+++ pages/_document.js
@@ -10,6 +10,7 @@ export default function Document() {
         <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
         <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
         <link rel="manifest" href="/site.webmanifest"></link>
+        <meta name="keywords" content="AS Creative,Creative Agency Buckinghamshire,Social Media Management Buckinghamshire,Social Media Management High Wycombe,Digital Marketing,Design Services,Customised Marketing Solutions,Individualised Marketing,Social Media Management,Social Media Coaching,Social Media Expertise,Professional Photography,Graphic Design,Branding Services,Personalised Marketing,Digital Design,Online Marketing,Business Branding,Content Creation,Visual Design,Social Media Strategy,Digital Media,Business Growth Solutions,Marketing Consultancy,Visual Branding,Online Presence,Social Media Training,Creative Solutions,Custom Graphics,Marketing and Design Expertise,Digital Advertising,Graphic Design Services,Photography Services,Content Strategy,Multimedia Design,Digital Strategy,Web Design,Logo Design,Social Media Optimization,Online Branding,Marketing Best Practices,Business Design Solutions,Tailored Marketing" />
       </Head>
       <body>
         <Main />
